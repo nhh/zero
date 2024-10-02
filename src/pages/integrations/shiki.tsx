89b4e9da278076a1f290d3774d2fc1203818b99a
nhh <niklas.hanft@gmail.com>
2024-10-02 23:22:39 +0200

    Relace dynamic imports with static

--- src/pages/integrations/shiki.tsx
+++ src/pages/integrations/shiki.tsx
@@ -1,17 +1,13 @@
 import { Header } from "../../components/header";
 import { Highlight } from "../../components/highlight";
+import * as exampleCss from "../../blocks/example.shiki.css"
+import * as exampleTxt from "../../blocks/example.shiki.css"
 
 export const ShikiPage = () => (
     <section class="py-2">
         <Header pre="Integrations" title="Integrating syntax highlighting with shiki" subtitle="Write a custom vite plugin that resolves files at runtime to embedd generated html"></Header>
-
         <p>You need the following files</p>
-        <Highlight>{import("../../blocks/folder-structure.shiki.txt")}</Highlight>
-
-        <Highlight>{import("../../blocks/example.shiki.css")}</Highlight>
-        <Highlight>{import("../../blocks/example.shiki.css")}</Highlight>
-        <Highlight>{import("../../blocks/example.shiki.css")}</Highlight>
-        <Highlight>{import("../../blocks/example.shiki.css")}</Highlight>
-
+        <Highlight>{exampleTxt}</Highlight>
+        <Highlight>{exampleCss}</Highlight>
     </section>
 )
